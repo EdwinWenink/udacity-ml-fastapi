455d89cfce4c117fd5444d3e9947befcba31561f
Edwin Wenink <edwinwenink@hotmail.com>
2023-08-11 11:30:23 +0200

    Apply feature engineering on test data too

--- src/ml/data.py
+++ src/ml/data.py
@@ -1,5 +1,10 @@
+import pandas as pd
 import numpy as np
 from sklearn.preprocessing import LabelBinarizer, OneHotEncoder
+
+
+def load_csv(uri: str) -> pd.DataFrame:
+    return pd.read_csv(uri)
 
 
 def process_data(
@@ -54,7 +59,7 @@ def process_data(
     X_continuous = X.drop(*[categorical_features], axis=1)
 
     if training is True:
-        encoder = OneHotEncoder(sparse=False, handle_unknown="ignore")
+        encoder = OneHotEncoder(sparse_output=False, handle_unknown="ignore")
         lb = LabelBinarizer()
         X_categorical = encoder.fit_transform(X_categorical)
         y = lb.fit_transform(y.values).ravel()
--- src/train_model.py
+++ src/train_model.py
@@ -1,14 +1,20 @@
-# Script to train machine learning model.
+'''
+Script to train a simple machine learning model.
+'''
 
+import pandas as pd
 from sklearn.model_selection import train_test_split
 
-# Add the necessary imports for the starter code.
+from src.ml.data import load_csv, process_data
 
-# Add code to load in the data.
+
+# Load census data
+data = load_csv('data/census.csv')
 
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
 train, test = train_test_split(data, test_size=0.20)
 
+# Variables
 cat_features = [
     "workclass",
     "education",
@@ -19,10 +25,19 @@ cat_features = [
     "sex",
     "native-country",
 ]
+
+# Target label
+target_label = "salary"
+
+# Preprocessing
 X_train, y_train, encoder, lb = process_data(
-    train, categorical_features=cat_features, label="salary", training=True
+    train, categorical_features=cat_features, label=target_label, training=True
 )
 
-# Proces the test data with the process_data function.
+# Process the test data using the fitted OneHotEncoder and label binarizer
+X_test, y_test, _, _ = process_data(
+    test, categorical_features=cat_features, label=target_label,
+    training=False, encoder=encoder, lb=lb
+)
 
 # Train and save a model.
