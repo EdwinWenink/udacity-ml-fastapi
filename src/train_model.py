8fdfcc2d069c8e1be41ccdda0c8d1bd2bef9153c
Edwin Wenink <edwinwenink@hotmail.com>
2023-08-11 17:43:17 +0200

    Basic training script done

--- src/train_model.py
+++ src/train_model.py
@@ -1,20 +1,26 @@
 '''
 Script to train a simple machine learning model.
 '''
+import joblib
 
 import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, confusion_matrix
 
-from src.ml.data import load_csv, process_data
+from src.ml.data import load_csv, preprocessing, feature_engineering
 
 
 # Load census data
-data = load_csv('data/census.csv')
+df = load_csv('data/census.csv')
 
-# Optional enhancement, use K-fold cross validation instead of a train-test split.
-train, test = train_test_split(data, test_size=0.20)
+# Preprocess data
+df = preprocessing(df)
 
-# Variables
+# TODO Optional enhancement, use K-fold cross validation instead of a train-test split.
+df_train, df_test = train_test_split(df, test_size=0.20)
+
+# Categorical variables
 cat_features = [
     "workclass",
     "education",
@@ -30,14 +36,35 @@ cat_features = [
 target_label = "salary"
 
 # Preprocessing
-X_train, y_train, encoder, lb = process_data(
-    train, categorical_features=cat_features, label=target_label, training=True
+X_train, y_train, encoder, lb = feature_engineering(
+    df_train, categorical_features=cat_features, label=target_label, training=True
 )
 
 # Process the test data using the fitted OneHotEncoder and label binarizer
-X_test, y_test, _, _ = process_data(
-    test, categorical_features=cat_features, label=target_label,
+X_test, y_test, _, _ = feature_engineering(
+    df_test, categorical_features=cat_features, label=target_label,
     training=False, encoder=encoder, lb=lb
 )
 
+# Model parameters
+params = {
+    'n_estimators': 200
+}
+
+# Decision tree
+clf = RandomForestClassifier(**params)
+
 # Train and save a model.
+clf.fit(X_train, y_train)
+joblib.dump(clf, filename='model/random_forest.pkl')
+
+# Predict
+y_pred = clf.predict(X_test)
+
+# Confusion matrix
+cm = confusion_matrix(y_test, y_pred)
+print("Confusion matrix:\n", cm)
+
+# Classification_report
+report = classification_report(y_test, y_pred)
+print("\nClassification report:\n", report)
