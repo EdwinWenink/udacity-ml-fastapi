f8dbb13372391b8292edae94bfb7ac5a5673cc2c
Edwin Wenink <edwinwenink@hotmail.com>
2023-08-15 16:44:04 +0200

    Correct usage of aliases

--- main.py
+++ main.py
@@ -8,14 +8,15 @@ from enum import Enum
 from fastapi import FastAPI, Depends, HTTPException
 from pydantic import BaseModel, Field, validator, ConfigDict
 from sklearn.exceptions import InconsistentVersionWarning
+import pandas as pd
 
-from src.ml.data import feature_engineering
+from src.ml.data import preprocessing, feature_engineering
 from src.ml.model import inference, load_model
 
 
-def hyphen_to_underscore(string: str):
+def underscore_to_hyphen(string: str):
     """Helper function to replace hyphens with underscores."""
-    return string.replace('-', '_')
+    return string.replace('_', '-')
 
 
 class CensusData(BaseModel):
@@ -23,22 +24,18 @@ class CensusData(BaseModel):
     This class defines which data inputs are valid for our machine learning model.
     We provide an example input for each field.
     """
-
-    # input_cols: list = ['age', 'workclass', 'fnlgt', 'education', 'education-num', 'marital-status',
-    #  'occupation', 'relationship', 'race', 'sex', 'capital-gain', 'capital-loss', 'hours-per-week', 'native-country', 'salary']
-
     # Automatically generate aliases for names with hyphens where underscores are used instead
     # because hyphens cannot be used in field names in Python. In Pydantic v2 this would be:
     # model_config = ConfigDict(alias_generator=hyphen_to_underscore)
     # In v1 (1.10) this is written as:
     class Config:
-        alias_generator = hyphen_to_underscore
+        alias_generator = underscore_to_hyphen
 
     age: int = Field(..., example=39)
     workclass: str = Field(..., example='State-gov')
     fnlgt: int = Field(..., example=77516)
     education: str = Field(..., example="Bachelors")
-    education_num: int = Field(..., example=13)
+    education_num: int = Field(..., example=13)  # alias
     marital_status: str = Field(..., example="Never-married")  # alias
     occupation: str = Field(..., example="Adm-clerical")
     relationship: str = Field(..., example="Not-in-family")
@@ -50,7 +47,7 @@ class CensusData(BaseModel):
     native_country: str = Field(..., example="United-States")  # alias
     salary: str = Field(..., example="<=50K")
 
-    # NOTE in pydantic v1 this is called @validator I think
+    # NOTE in pydantic v2 this is called @field_validator
     @validator("age")
     @classmethod
     def valid_age(cls, value):
@@ -79,6 +76,7 @@ app = FastAPI(
 
 try:
     # TODO use DVC; this model is not yet available in the repo
+    # NOTE this object should include required data transforms
     CLF = load_model('model/random_forest.pkl')
 except InconsistentVersionWarning as warning:
     print(warning.original_sklearn_version)
@@ -89,29 +87,19 @@ async def get_root():
     return "Hello there!"
 
 
-# TODO can we do post on root too?
 @app.post("/inference/")
-async def get_predictions(data: CensusData):
+async def get_prediction(data: CensusData):
 
-    print(data)
-    print(type(data))
-    # TODO repeat correct preprocessing and feature engineering
-    # as done during training (incl. label encoder etc.)
-    '''
+    # NOTE we have to use the alias here!
+    # In Pydantic v2, use model_dump(by_alias=True)
+    d = data.dict(by_alias=True)
+
+    # For a single data point we need to manually pass an index to construct a dataframe
+    df = pd.DataFrame.from_records(d, index=[0])
+
+    # Repeat basic data cleaning
     df = preprocessing(df)
 
-    # Process the test data using the fitted OneHotEncoder and label binarizer
-    X_test, y_test, _, _ = feature_engineering(
-        df_test, categorical_features=cat_features, label=target_label,
-        training=False, encoder=encoder, lb=lb
-    )
-    '''
-
-    # clf: fixed? Use getter?
-    # Or allow model selection?
-    # y = inference(CLF, data)
-
-    y = [0, 1]
-    # return y
-
-    return data
+    # Perform inference. The inference artifact includes data transforms.
+    y = inference(CLF, df)
+    return int(y)
